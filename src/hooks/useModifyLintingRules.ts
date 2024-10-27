6936938e30dbffa7b61a642eed7eb7af7a827aa2
matias <mchialva@mail.austral.edu.ar>
2024-10-27 10:30:36 -0300

    modify linting rules hook.

--- src/hooks/useModifyLintingRules.ts
+++ src/hooks/useModifyLintingRules.ts
@@ -5,7 +5,7 @@ export function useModifyLintingRules() {
     const modifyRules = async (newRules: Rule[], token: string): Promise<Rule[]> => {
         try {
             const response = await axios.post(
-                "/  lint/rules",
+                "/lint/rules",
                 { rules: newRules },
                 {
                     headers: {

1d202890679947cc886ffc31d6958f7d03d2e6d0
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-11-13 23:39:12 -0300

    Merge

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const FRONTEND_URL = process?.env?.FRONTEND_URL ?? "http://localhost:5173"
-export const BACKEND_URL = process?.env?.BACKEND_URL ?? "http://localhost/api"
+export const FRONTEND_URL = process?.env?.FRONTEND_URL ?? "https://ingsis-students-prod.duckdns.org"
+export const BACKEND_URL = process?.env?.BACKEND_URL ?? "https://ingsis-students.duckdns-prod.org/api"
 export const AUTH0_USERNAME = process?.env?.AUTH0_USERNAME ?? "nachochevamusica@gmail.com"
 export const AUTH0_PASSWORD = process?.env?.AUTH0_PASSWORD ?? "Testpassword1!"
